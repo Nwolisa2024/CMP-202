f4524cba6bc56c7da1e214ed9e33bb26faf71c65
Nwolisa2024 <159129263+Nwolisa2024@users.noreply.github.com>
2024-04-30 22:05:31 +0100

    Create Admin.java

---
+++ Admin.java
@@ -0,0 +1,14 @@
+package OOP;
+
+public class Admin extends User {
+    String staffNo;
+    void uploadResults(String course, float score) {
+        System.out.println(course + " " + score);
+    }
+    public static void main(String[] args) {
+        Admin adminName = new Admin();
+        adminName.name ="Daniel";
+        adminName.password = "88888";
+        System.out.println(adminName.register());
+    }
+} 
